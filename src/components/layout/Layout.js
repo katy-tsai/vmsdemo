9af9e4601f5c6eb55bbd6a38b90e81319211939f
katy <katytsai.git@gmail.com>
2020-11-05 22:39:00 +0800

    grid

--- src/components/layout/Layout.js
+++ src/components/layout/Layout.js
@@ -3,14 +3,19 @@ import Header from './Header';
 import Sidebar from '../Sidebar/Sidebar';
 const Layout = (props) => {
     return (
-        <div className="wrapper">
-            <Sidebar />
+        <div className="container">
+            {/* <Sidebar />
             <div className="main-container">
                 <Header />
                 <div className="main">
                 {props.children}
                 </div>               
+            </div> */}
+            <div className="row">
+            <div className=" col-3">sidebar</div>
+            <div className=" col-9">main</div>
             </div>
+           
         </div>
     );
 };
